08f73c5414833e08fed92b3a4f6f9b2fc928e627
training <training@PC401993.cts.com>
2019-12-23 12:24:21 +0530

    second commit

--- helloworld.java
+++ helloworld.java
@@ -2,5 +2,8 @@ class helloworld{
 
         public static void main(String args[]){
                 System.out.println("Hello hi welcome");
+                for(int i=1; i<=10; i++){
+                System.out.println(i++ "chennai");
+                }
         }
 }
